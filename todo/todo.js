a83e7d9de1d9cef8123d3c9c8f948d891dff3bde
Shawn Biddle <shawncplus@gmail.com>
2015-09-27 21:49:10 -0400

    Implement firebase

--- todo/todo.js
+++ todo/todo.js
@@ -3,6 +3,9 @@ $(function () {
 
     var ENTER_KEY = 13;
 
+    // Make the user log in
+    TodoList.auth();
+
     $('#todoAdd').on('click', function () {
         var $descInput = $('#todoDesc');
         var value = $descInput.val();
@@ -11,13 +14,13 @@ $(function () {
             return;
         }
 
-        var task = {
+        var todo = {
             description: value,
             priority: 'medium',
-            done: false
+            completed: false
         };
 
-        TodoList.addTask(task);
+        TodoList.addTask(todo);
         $descInput.val('').focus();
     });
 
@@ -30,19 +33,64 @@ $(function () {
 
 
 var TodoList = {
-    cursor: 0,
-    todos: [],
+    firebase: new Firebase("https://boiling-heat-4673.firebaseio.com"),
 
-    addTask: function (task) {
-        task.id = TodoList.cursor++;
-        // Add the task to the data list
-        this.todos.push(task);
+    auth: function () {
+        // login with google and get our todo list
+        this.firebase.authWithOAuthPopup("google", function(error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                $('body').removeClass('auth');
+                this.todos = this.firebase.child('users/' + authData.uid);
+            }
 
+            this.init();
+        }.bind(this));
+    },
+
+    init: function () {
+        // Any time our todolist data changes pull down the new data and re-render the list
+        this.todos.on("value", function (snapshot) {
+            $('#todoList').empty();
+            snapshot.forEach(function (todo) {
+                this._renderTask(todo.val(), todo);
+            }.bind(this));
+        }.bind(this));
+    },
+
+    addTask: function (todo) {
+        this.todos.push(todo);
+    },
+
+    remove: function (id) {
+        this.todos.child(id).remove();
+    },
+
+    /**
+     * Toggle a given todo's complete state
+     * @param {String} id
+     */
+    toggleComplete: function (id) {
+        var todoRef = this.todos.child(id) ;
+        todoRef.once('value', function (snapshot) {
+            var todo = snapshot.val();
+            todoRef.child('completed').set(!todo.completed);
+        });
+    },
+
+    /**
+     * Create the actual html for a todo
+     * @param {Object} todo
+     * @param {DataSnapshot} ref
+     */
+    _renderTask: function (todo, ref) {
         // create todo item and add to the DOM
         var $todoList = $('#todoList');
         var $todo = $('<li>');
-        $todo.data('id', task.id);
-        $todo.addClass('priority-' + task.priority);
+        $todo.data('id', ref.key());
+        $todo.toggleClass('completed', todo.completed);
+        $todo.addClass('priority-' + todo.priority);
 
         // setup complete toggle
         var $completeCheck = $('<input type="checkbox" class="complete-check">');
@@ -51,14 +99,14 @@ var TodoList = {
             $todo.toggleClass('completed');
             TodoList.toggleComplete($todo.data('id'));
         });
-
         $todo.append($completeCheck);
 
         // add description
         $todo.append(
-            $('<label class="description">' + task.description + '</label>')
+            $('<label class="description">' + todo.description + '</label>')
         );
 
+        // setup delete button
         var $deleteBtn = $('<button class="remove">&times;</button>');
         $deleteBtn.on('click', function (e) {
             var $todo = $(e.target).closest('li');
@@ -67,30 +115,8 @@ var TodoList = {
         });
         $todo.append($deleteBtn);
 
+        // finally add it to the list of todos
         $todo.appendTo($todoList);
-    },
-
-    toggleComplete: function (id) {
-        var index = this._indexForId(id);
-        var task = this.todos[index];
-        task.completed = !task.completed;
-    },
-
-    remove: function (id) {
-        var index = this._indexForId(id);
-        this.todos.splice(index, 1);
-    },
-
-    _indexForId: function (id) {
-        var index = null;
-        this.todos.some(function (todo, todoIndex) {
-            if (todo.id === id) {
-                index = todoIndex;
-                return true;
-            }
-        });
-
-        return index;
     }
 };
 
